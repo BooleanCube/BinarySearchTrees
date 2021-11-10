093edfe022d662c6c768139109153d7c1ad8b806
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2021-11-10 08:22:37 -0500

    Update BalancingAct.java

--- BalancingAct.java
+++ BalancingAct.java
@@ -22,28 +22,82 @@ public class BalancingAct {
             String[] split = bf.readLine().split(" ", 2);
             int k = Integer.parseInt(split[0]);
             String s = split[1];
+            ArrayList<BST> trees = new ArrayList<>();
+            for(int i=0; i<k; i++) {
+            	Node head = new Node(s.substring(i,i+1));
+            	trees.add(new BST(head));
+            }
+            for(int idx=k; idx<s.length(); idx++) {
+            	Node node = new Node(s.substring(idx,idx+1));
+            	int minDepth = Integer.MAX_VALUE;
+                BST treeMinDepth = null;
+                for(BST tree : trees) {
+                	int depth = tree.getDepth(tree, node);
+                	if(minDepth > depth) {
+                		minDepth = depth;
+                		treeMinDepth = tree;
+                	}
+                }
+                treeMinDepth.addNode(node);
+            }
+            System.out.println();
         }
 
     }
 
 }
 
-class BinarySearchTree {
+class BST {
     
-    public BSTNode head;
+    public Node head;
 
-    public BinarySearchTree(BSTNode head) {
+    public BST(Node head) {
         this.head = head;
+    }
+    
+    public int addNode(Node node) {
+    	
+    	return -1;
+    }
+    
+    public int getDepth(BST tree, Node node) {
+    	Node current = tree.head;
+    	int depth = 1;
+    	while(true) {
+    		if(current.compareTo(node) == 1) {
+    			if(current.left == null) return depth;
+    			else {
+    				++depth;
+    				current = current.left;
+    			}
+    		}
+    		if(current.compareTo(node) < 1) {
+    			if(current.right == null) return depth;
+    			else {
+    				++depth;
+    				current = current.right;
+    			}
+    		}
+    	}
+    }
+    
+    public String preOrder(BST tree) {
+    	StringBuilder bs = new StringBuilder();
+    	ArrayDeque<Node> q = new ArrayDeque<>();
+    	q.add(tree.head);
+    	while(!q.isEmpty()) {
+    		
+    	}
     }
 
 }
 
-class BSTNode implements Comparable<BSTNode> {
+class Node implements Comparable<Node> {
     
     String name;
-    public BSTNode left, right;
+    public Node left, right;
 
-    public BSTNode(String s) {
+    public Node(String s) {
         this.name = s;
         this.left = null;
         this.right = null;
@@ -53,9 +107,8 @@ class BSTNode implements Comparable<BSTNode> {
         return name;
     }
 
-    public int compareTo(BSTNode other) {
+    public int compareTo(Node other) {
         return this.name.compareTo(other.name);
     }
 
 }
-
