7401b768326888e8a4e1cd80412857c6f606de5f
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2021-11-11 13:07:51 -0500

    class name

--- GeneralToBST.java
+++ GeneralToBST.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class GTB {
+public class GeneralToBST {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
